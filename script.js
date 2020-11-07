3620d7e500d7c32dfd7f8bad92516d3ef85c1d93
Alois <aloisdegouvello@live.fr>
2020-11-07 19:40:04 +0100

    Ignore empty lines

--- script.js
+++ script.js
@@ -204,7 +204,7 @@ function cleanErrorList() {
 const keywords = ["Deck", "Sideboard", "Maybeboard"];
 function fill(value, isToken = false) {
   [...value.split("\n")]
-    .filter((line) => !keywords.includes(line.trim()))
+    .filter((line) => !keywords.includes(line.trim()) || !!line.trim())
     .forEach((context) => {
       const card = parseContext(context);
       if (isUrl(card.name)) {
